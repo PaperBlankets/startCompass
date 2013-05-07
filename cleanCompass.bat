2b969f932c93d1d1b2c8d7669afb7554b6a6fe27
PaperBlankets <GSG-9@mediocrocy.com>
2013-05-07 13:10:10 -0500

    Script closes when complete. Better relative path structure.

--- cleanCompass.bat
+++ cleanCompass.bat
@@ -1,12 +1,14 @@
-:: Batch Script to compass clean all Compass Directories
+:: Batch Script to compass watch all Compass Directories
 setlocal EnableDelayedExpansion
 ::Specifiy which locations to watch ONE location per line, no comments.
-set file=C:\altiris\locations.txt
+set file=%~dp0\locations.txt
 ::Specifiy Ruby location
 set rubyLocation=C:\Ruby200-x64\bin\
-::Iterate clear locations
+::Iterate watch locations
 FOR /F %%i IN (%file%) DO (
-  set watchLocation=%%i
-	::Call clear and send it current watch location, and ruby location
+	set watchLocation=%%i
+	::Call watch and send it current watch location, and ruby location
 	start cleanDirectory.CMD %watchLocation%
 )
+
+EXIT
